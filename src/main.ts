f8cd72bac36658ac07adb71cc2b18ff0e57f8788
Jr-01 <Jr-01@DESKTOP-BS7OJF3>
2020-10-19 11:16:57 +0800

    -push merge

--- src/main.ts
+++ src/main.ts
@@ -4,16 +4,13 @@ import router from './router'
 import store from './store'
 import service from "./utils/api/https"
 import urls from "./utils/api/urls"
-<<<<<<< HEAD
 import DefaultLayout from './layout/default.vue'
 import DefaultRedirect from './layout/redirect.vue'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
 import '@mdi/font/css/materialdesignicons.css'
-=======
 import { OpenIdConnectService } from './services/auth/openIdConnectService'
->>>>>>> 039a8b919b18a494d36c300eca3b204a0d55c6bb
 
 Vue.component('default-layout',DefaultLayout)
 Vue.component('redirect-layout',DefaultRedirect)
