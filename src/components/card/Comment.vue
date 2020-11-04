1af87fc91c47b359d8406ec64fc7ffd2595605f0
Jr-01 <Jr-01@DESKTOP-BS7OJF3>
2020-11-04 16:10:04 +0800

    -push comment

--- src/components/card/Comment.vue
+++ src/components/card/Comment.vue
@@ -1,35 +1,37 @@
 <template>
     <v-row no-gutters>
-        <v-col>
-        <v-list expand="true">
-            <v-list-item>
-                <v-list-item-avatar color="grey darken-3">
-                <v-img
-                    class="elevation-6"
-                    alt=""
-                    src="https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light"
-                ></v-img>
-                </v-list-item-avatar>
+        <v-flex>
+            <v-list three-line style="padding:3px;">
+                <v-list-item>
+                    <v-list-item-avatar size="32" color="grey darken-3">
+                    <v-img
+                        class="elevation-6"
+                        alt=""
+                        src="https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light"
+                    ></v-img>
+                    </v-list-item-avatar>
 
-                <v-list-item-content>
-                    <v-card class="rounded-xl" color="darken-4"  outlined>
-                        <v-list-item>
-                                <v-list-item-content >
-                                    <v-list-item-title >
-                                        Micker
-                                    </v-list-item-title>
-                                    <v-list-item-subtitle class="wrap-text">
-                                        大家好我是變態老大家好我是變態老大家好我是變態老大家好我是變態老大大家好我是變態老大家好我是變態老大家好我是變態老大家好我是變態老大大家好我是變態老大家好我是變態老大家好我是變態老大家好我是變態老大
-                                    </v-list-item-subtitle>
+                    <v-list-item-content style="padding:0px;">
+                        <v-card class="rounded-xl" color=" darken-4" outlined>
+                            <v-list three-line style="padding:0px;">
+                                <v-list-item  style="padding:3px;">
+                                        <v-list-item-content >
+                                            <v-list-item-title >
+                                                Micker
+                                            </v-list-item-title>
+                                            <v-list-item-subtitle class="wrap-text">
+                                                大家好我是變態老大家好我是變態老大家好我是變態老大家好我是變態老大大家好我是變態老大家好我是變態老大家好我是變態老大家好我是變態老大大家好我是變態老大家好我是變態老大家好我是變態老大家好我是變態老大
+                                            </v-list-item-subtitle>
 
-                                </v-list-item-content>
-                        </v-list-item>
-                    </v-card>
-                    
-                </v-list-item-content>
-                </v-list-item>
-        </v-list>
-        </v-col>
+                                        </v-list-item-content>
+                                </v-list-item>
+                            </v-list>
+                        </v-card>
+                        
+                    </v-list-item-content>
+                    </v-list-item>
+            </v-list>
+        </v-flex>
     </v-row>
 </template>
 
@@ -53,6 +55,8 @@ export default class Comment extends Vue {
 }
 </script>
 
-<style scoped>
-
+<style >
+    .wrap-text {
+    -webkit-line-clamp: unset !important;
+    }
 </style>
